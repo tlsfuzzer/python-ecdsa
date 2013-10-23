3a15318fd9c2ab6bb558cbddb67fbf1757bd3766
Brian Warner <warner@lothar.com>
2013-10-23 11:35:46 -0700

    make SECP256K1 visible in __init__.py, release 0.10

--- ecdsa/__init__.py
+++ ecdsa/__init__.py
@@ -1,10 +1,10 @@
 __all__ = ["curves", "der", "ecdsa", "ellipticcurve", "keys", "numbertheory",
            "test_pyecdsa", "util", "six"]
 from .keys import SigningKey, VerifyingKey, BadSignatureError, BadDigestError
-from .curves import NIST192p, NIST224p, NIST256p, NIST384p, NIST521p
+from .curves import NIST192p, NIST224p, NIST256p, NIST384p, NIST521p, SECP256k1
 
 _hush_pyflakes = [SigningKey, VerifyingKey, BadSignatureError, BadDigestError,
-                  NIST192p, NIST224p, NIST256p, NIST384p, NIST521p]
+                  NIST192p, NIST224p, NIST256p, NIST384p, NIST521p, SECP256k1]
 del _hush_pyflakes
 
 # This code comes from http://github.com/warner/python-ecdsa
