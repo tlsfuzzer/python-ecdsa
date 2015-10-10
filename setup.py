5fbc2434e903674500f46ca6a0f8923a53d5bc0d
Hubert Kario <hkario@redhat.com>
2015-10-10 15:46:32 +0200

    Make Python 3.5 support official

--- setup.py
+++ setup.py
@@ -93,5 +93,6 @@ setup(name="ecdsa",
           "Programming Language :: Python :: 3.2",
           "Programming Language :: Python :: 3.3",
           "Programming Language :: Python :: 3.4",
+          "Programming Language :: Python :: 3.5",
       ],
 )
