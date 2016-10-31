99ecf7038c4053be69e4de07d774fcc92a4775ce
bhate <sb@axiros.com>
2016-10-31 18:28:07 +0100

    Added support for Brainpool r1

--- src/ecdsa/test_pyecdsa.py
+++ src/ecdsa/test_pyecdsa.py
@@ -22,7 +22,11 @@ from .util import number_to_string, encoded_oid_ecPublicKey, \
         MalformedSignature
 from .curves import Curve, UnknownCurveError
 from .curves import NIST192p, NIST224p, NIST256p, NIST384p, NIST521p, \
-        SECP256k1, curves
+        SECP256k1, BRAINPOOLP160r1, BRAINPOOLP192r1, BRAINPOOLP224r1, \
+        BRAINPOOLP256r1, BRAINPOOLP320r1, BRAINPOOLP384r1, BRAINPOOLP512r1, \
+        curves
+from .ecdsa import curve_brainpoolp224r1, curve_brainpoolp256r1, \
+        curve_brainpoolp384r1, curve_brainpoolp512r1
 from .ellipticcurve import Point
 from . import der
 from . import rfc6979
@@ -98,7 +102,10 @@ class ECDSA(unittest.TestCase):
         self.assertEqual(len(sig), default.signature_length)
         if BENCH:
             print_()
-        for curve in (NIST192p, NIST224p, NIST256p, NIST384p, NIST521p):
+        for curve in (NIST192p, NIST224p, NIST256p, NIST384p, NIST521p,
+                BRAINPOOLP160r1, BRAINPOOLP192r1, BRAINPOOLP224r1,
+                BRAINPOOLP256r1, BRAINPOOLP320r1, BRAINPOOLP384r1,
+                BRAINPOOLP512r1):
             start = time.time()
             priv = SigningKey.generate(curve=curve)
             pub1 = priv.get_verifying_key()
@@ -230,6 +237,19 @@ class ECDSA(unittest.TestCase):
         priv2 = SigningKey.from_der(s1)
         self.assertTruePrivkeysEqual(priv1, priv2)
 
+        priv1 = SigningKey.generate(curve=BRAINPOOLP512r1)
+        s1 = priv1.to_pem()
+        self.assertEqual(type(s1), binary_type)
+        self.assertTrue(s1.startswith(b("-----BEGIN EC PRIVATE KEY-----")))
+        self.assertTrue(s1.strip().endswith(b("-----END EC PRIVATE KEY-----")))
+        priv2 = SigningKey.from_pem(s1)
+        self.assertTruePrivkeysEqual(priv1, priv2)
+
+        s1 = priv1.to_der()
+        self.assertEqual(type(s1), binary_type)
+        priv2 = SigningKey.from_der(s1)
+        self.assertTruePrivkeysEqual(priv1, priv2)
+
     def assertTruePubkeysEqual(self, pub1, pub2):
         self.assertEqual(pub1.pubkey.point, pub2.pubkey.point)
         self.assertEqual(pub1.pubkey.generator, pub2.pubkey.generator)
@@ -250,6 +270,14 @@ class ECDSA(unittest.TestCase):
         self.assertEqual(type(s1), binary_type)
         self.assertEqual(len(s1), NIST256p.verifying_key_length)
         pub2 = VerifyingKey.from_string(s1, curve=NIST256p)
+        self.assertTruePubkeysEqual(pub1, pub2)
+
+        priv1 = SigningKey.generate(curve=BRAINPOOLP512r1)
+        pub1 = priv1.get_verifying_key()
+        s1 = pub1.to_string()
+        self.assertEqual(type(s1), binary_type)
+        self.assertEqual(len(s1), BRAINPOOLP512r1.verifying_key_length)
+        pub2 = VerifyingKey.from_string(s1, curve=BRAINPOOLP512r1)
         self.assertTruePubkeysEqual(pub1, pub2)
 
         pub1_der = pub1.to_der()
@@ -751,6 +779,43 @@ class OpenSSL(unittest.TestCase):
     def test_from_openssl_secp256k1(self):
         return self.do_test_from_openssl(SECP256k1)
 
+    @pytest.mark.skipif("brainpoolP160r1" not in OPENSSL_SUPPORTED_CURVES,
+                        reason="system openssl does not support brainpoolP160r1")
+    def test_from_openssl_brainpoolp160r1(self):
+        return self.do_test_from_openssl(BRAINPOOLP160r1)
+
+    @pytest.mark.skipif("brainpoolP192r1" not in OPENSSL_SUPPORTED_CURVES,
+                        reason="system openssl does not support brainpoolP192r1")
+    def test_from_openssl_brainpoolp192r1(self):
+        return self.do_test_from_openssl(BRAINPOOLP192r1)
+
+    @pytest.mark.skipif("brainpoolP224r1" not in OPENSSL_SUPPORTED_CURVES,
+                        reason="system openssl does not support brainpoolP224r1")
+    def test_from_openssl_brainpoolp224r1(self):
+        return self.do_test_from_openssl(BRAINPOOLP224r1)
+
+    @pytest.mark.skipif("brainpoolP256r1" not in OPENSSL_SUPPORTED_CURVES,
+                        reason="system openssl does not support brainpoolP256r1")
+    def test_from_openssl_brainpoolp256r1(self):
+        return self.do_test_from_openssl(BRAINPOOLP256r1)
+
+    @pytest.mark.skipif("brainpoolP320r1" not in OPENSSL_SUPPORTED_CURVES,
+                        reason="system openssl does not support brainpoolP320r1")
+    def test_from_openssl_brainpoolp320r1(self):
+        return self.do_test_from_openssl(BRAINPOOLP320r1)
+
+    @pytest.mark.skipif("brainpoolP384r1" not in OPENSSL_SUPPORTED_CURVES,
+                        reason="system openssl does not support brainpoolP384r1")
+    def test_from_openssl_brainpoolp384r1(self):
+        return self.do_test_from_openssl(BRAINPOOLP384r1)
+
+    @pytest.mark.skipif("brainpoolP512r1" not in OPENSSL_SUPPORTED_CURVES,
+                        reason="system openssl does not support brainpoolP512r1")
+    def test_from_openssl_brainpoolp512r1(self):
+        return self.do_test_from_openssl(BRAINPOOLP512r1)
+
+    @pytest.mark.skipif("" not in OPENSSL_SUPPORTED_CURVES,
+                        reason="system openssl does not support ")
     def do_test_from_openssl(self, curve):
         curvename = curve.openssl_name
         assert curvename
@@ -810,6 +875,41 @@ class OpenSSL(unittest.TestCase):
                         reason="system openssl does not support secp256k1")
     def test_to_openssl_secp256k1(self):
         self.do_test_to_openssl(SECP256k1)
+
+    @pytest.mark.skipif("brainpoolP160r1" not in OPENSSL_SUPPORTED_CURVES,
+                        reason="system openssl does not support brainpoolP160r1")
+    def test_to_openssl_brainpoolp160r1(self):
+        self.do_test_to_openssl(BRAINPOOLP160r1)
+
+    @pytest.mark.skipif("brainpoolP192r1" not in OPENSSL_SUPPORTED_CURVES,
+                        reason="system openssl does not support brainpoolP192r1")
+    def test_to_openssl_brainpoolp192r1(self):
+        self.do_test_to_openssl(BRAINPOOLP192r1)
+
+    @pytest.mark.skipif("brainpoolP224r1" not in OPENSSL_SUPPORTED_CURVES,
+                        reason="system openssl does not support brainpoolP224r1")
+    def test_to_openssl_brainpoolp224r1(self):
+        self.do_test_to_openssl(BRAINPOOLP224r1)
+
+    @pytest.mark.skipif("brainpoolP256r1" not in OPENSSL_SUPPORTED_CURVES,
+                        reason="system openssl does not support brainpoolP256r1")
+    def test_to_openssl_brainpoolp256r1(self):
+        self.do_test_to_openssl(BRAINPOOLP256r1)
+
+    @pytest.mark.skipif("brainpoolP320r1" not in OPENSSL_SUPPORTED_CURVES,
+                        reason="system openssl does not support brainpoolP320r1")
+    def test_to_openssl_brainpoolp320r1(self):
+        self.do_test_to_openssl(BRAINPOOLP320r1)
+
+    @pytest.mark.skipif("brainpoolP384r1" not in OPENSSL_SUPPORTED_CURVES,
+                        reason="system openssl does not support brainpoolP384r1")
+    def test_to_openssl_brainpoolp384r1(self):
+        self.do_test_to_openssl(BRAINPOOLP384r1)
+
+    @pytest.mark.skipif("brainpoolP512r1" not in OPENSSL_SUPPORTED_CURVES,
+                        reason="system openssl does not support brainpoolP512r1")
+    def test_to_openssl_brainpoolp512r1(self):
+        self.do_test_to_openssl(BRAINPOOLP512r1)
 
     def do_test_to_openssl(self, curve):
         curvename = curve.openssl_name
@@ -1094,6 +1194,190 @@ class RFC6979(unittest.TestCase):
             hsh=sha512(b("test")).digest(),
             hash_func=sha512,
             expected=int("16200813020EC986863BEDFC1B121F605C1215645018AEA1A7B215A564DE9EB1B38A67AA1128B80CE391C4FB71187654AAA3431027BFC7F395766CA988C964DC56D", 16))
+
+class ECDH(unittest.TestCase):
+    def _do(self, curve, generator, dA, x_qA, y_qA, dB, x_qB, y_qB, x_Z, y_Z):
+        qA = dA * generator
+        qB = dB * generator
+        Z = dA * qB
+        self.assertEqual(Point(curve, x_qA, y_qA), qA)
+        self.assertEqual(Point(curve, x_qB, y_qB), qB)
+        self.assertTrue((dA * qB) ==
+                        (dA * dB * generator) ==
+                        (dB * dA * generator) ==
+                        (dB * qA))
+        self.assertEqual(Point(curve, x_Z, y_Z), Z)
+
+
+class RFC6932(ECDH):
+    # https://tools.ietf.org/html/rfc6932#appendix-A.1
+
+    def test_brainpoolP224r1(self):
+        self._do(
+            curve=curve_brainpoolp224r1,
+            generator=BRAINPOOLP224r1.generator,
+            dA=int("7C4B7A2C8A4BAD1FBB7D79CC0955DB7C6A4660CA64CC4778159B495E",
+                   16),
+            x_qA=int("B104A67A6F6E85E14EC1825E1539E8ECDBBF584922367DD88C6BDCF2",
+                     16),
+            y_qA=int("46D782E7FDB5F60CD8404301AC5949C58EDB26BC68BA07695B750A94",
+                     16),
+            dB=int("63976D4AAE6CD0F6DD18DEFEF55D96569D0507C03E74D6486FFA28FB",
+                   16),
+            x_qB=int("2A97089A9296147B71B21A4B574E1278245B536F14D8C2B9D07A874E",
+                     16),
+            y_qB=int("9B900D7C77A709A797276B8CA1BA61BB95B546FC29F862E44D59D25B",
+                     16),
+            x_Z=int("312DFD98783F9FB77B9704945A73BEB6DCCBE3B65D0F967DCAB574EB",
+                    16),
+            y_Z=int("6F800811D64114B1C48C621AB3357CF93F496E4238696A2A012B3C98",
+                    16))
+
+    def test_brainpoolP256r1(self):
+        self._do(
+            curve=curve_brainpoolp256r1,
+            generator=BRAINPOOLP256r1.generator,
+            dA=int("041EB8B1E2BC681BCE8E39963B2E9FC415B05283313DD1A8BCC055F11AE"
+                   "49699", 16),
+            x_qA=int("78028496B5ECAAB3C8B6C12E45DB1E02C9E4D26B4113BC4F015F60C5C"
+                     "CC0D206", 16),
+            y_qA=int("A2AE1762A3831C1D20F03F8D1E3C0C39AFE6F09B4D44BBE80CD100987"
+                     "B05F92B", 16),
+            dB=int("06F5240EACDB9837BC96D48274C8AA834B6C87BA9CC3EEDD81F99A16B8D"
+                   "804D3", 16),
+            x_qB=int("8E07E219BA588916C5B06AA30A2F464C2F2ACFC1610A3BE2FB240B635"
+                     "341F0DB", 16),
+            y_qB=int("148EA1D7D1E7E54B9555B6C9AC90629C18B63BEE5D7AA6949EBBF47B2"
+                     "4FDE40D", 16),
+            x_Z=int("05E940915549E9F6A4A75693716E37466ABA79B4BF2919877A16DD2CC2"
+                    "E23708", 16),
+            y_Z=int("6BC23B6702BC5A019438CEEA107DAAD8B94232FFBBC350F3B137628FE6"
+                    "FD134C", 16))
+
+    def test_brainpoolP384r1(self):
+        self._do(
+            curve=curve_brainpoolp384r1,
+            generator=BRAINPOOLP384r1.generator,
+            dA=int("014EC0755B78594BA47FB0A56F6173045B4331E74BA1A6F47322E70D79D"
+                   "828D97E095884CA72B73FDABD5910DF0FA76A", 16),
+            x_qA=int("45CB26E4384DAF6FB776885307B9A38B7AD1B5C692E0C32F012533277"
+                     "8F3B8D3F50CA358099B30DEB5EE69A95C058B4E", 16),
+            y_qA=int("8173A1C54AFFA7E781D0E1E1D12C0DC2B74F4DF58E4A4E3AF7026C5D3"
+                     "2DC530A2CD89C859BB4B4B768497F49AB8CC859", 16),
+            dB=int("6B461CB79BD0EA519A87D6828815D8CE7CD9B3CAA0B5A8262CBCD550A01"
+                   "5C90095B976F3529957506E1224A861711D54", 16),
+            x_qB=int("01BF92A92EE4BE8DED1A911125C209B03F99E3161CFCC986DC7711383"
+                     "FC30AF9CE28CA3386D59E2C8D72CE1E7B4666E8", 16),
+            y_qB=int("3289C4A3A4FEE035E39BDB885D509D224A142FF9FBCC5CFE5CCBB3026"
+                     "8EE47487ED8044858D31D848F7A95C635A347AC", 16),
+            x_Z=int("04CC4FF3DCCCB07AF24E0ACC529955B36D7C807772B92FCBE48F3AFE9A"
+                    "2F370A1F98D3FA73FD0C0747C632E12F1423EC", 16),
+            y_Z=int("7F465F90BD69AFB8F828A214EB9716D66ABC59F17AF7C75EE7F1DE22AB"
+                    "5D05085F5A01A9382D05BF72D96698FE3FF64E", 16))
+
+    def test_brainpoolP512r1(self):
+        self._do(
+            curve=curve_brainpoolp512r1,
+            generator=BRAINPOOLP512r1.generator,
+            dA=int("636B6BE0482A6C1C41AA7AE7B245E983392DB94CECEA2660A379CFE1595"
+                   "59E357581825391175FC195D28BAC0CF03A7841A383B95C262B98378287"
+                   "4CCE6FE333", 16),
+            x_qA=int("0562E68B9AF7CBFD5565C6B16883B777FF11C199161ECC427A39D17EC"
+                     "2166499389571D6A994977C56AD8252658BA8A1B72AE42F4FB7532151"
+                     "AFC3EF0971CCDA", 16),
+            y_qA=int("A7CA2D8191E21776A89860AFBC1F582FAA308D551C1DC6133AF9F9C3C"
+                     "AD59998D70079548140B90B1F311AFB378AA81F51B275B2BE6B7DEE97"
+                     "8EFC7343EA642E", 16),
+            dB=int("0AF4E7F6D52EDD52907BB8DBAB3992A0BB696EC10DF11892FF205B66D38"
+                   "1ECE72314E6A6EA079CEA06961DBA5AE6422EF2E9EE803A1F236FB96A17"
+                   "99B86E5C8B", 16),
+            x_qB=int("5A7954E32663DFF11AE24712D87419F26B708AC2B92877D6BFEE2BFC4"
+                     "3714D89BBDB6D24D807BBD3AEB7F0C325F862E8BADE4F74636B97EAAC"
+                     "E739E11720D323", 16),
+            y_qB=int("96D14621A9283A1BED84DE8DD64836B2C0758B11441179DC0C54C0D49"
+                     "A47C03807D171DD544B72CAAEF7B7CE01C7753E2CAD1A861ECA55A719"
+                     "54EE1BA35E04BE", 16),
+            x_Z=int("1EE8321A4BBF93B9CF8921AB209850EC9B7066D1984EF08C2BB7232362"
+                    "08AC8F1A483E79461A00E0D5F6921CE9D360502F85C812BEDEE23AC5B2"
+                    "10E5811B191E", 16),
+            y_Z=int("2632095B7B936174B41FD2FAF369B1D18DCADEED7E410A7E251F083109"
+                    "7C50D02CFED02607B6A2D5ADB4C0006008562208631875B58B54ECDA5A"
+                    "4F9FE9EAABA6", 16))
+
+
+class RFC7027(ECDH):
+    # https://tools.ietf.org/html/rfc7027#appendix-A
+
+    def test_brainpoolP256r1(self):
+        self._do(
+            curve=curve_brainpoolp256r1,
+            generator=BRAINPOOLP256r1.generator,
+            dA=int("81DB1EE100150FF2EA338D708271BE38300CB54241D79950F77B0630398"
+                   "04F1D", 16),
+            x_qA=int("44106E913F92BC02A1705D9953A8414DB95E1AAA49E81D9E85F929A8E"
+                     "3100BE5", 16),
+            y_qA=int("8AB4846F11CACCB73CE49CBDD120F5A900A69FD32C272223F789EF10E"
+                     "B089BDC", 16),
+            dB=int("55E40BC41E37E3E2AD25C3C6654511FFA8474A91A0032087593852D3E7D"
+                   "76BD3", 16),
+            x_qB=int("8D2D688C6CF93E1160AD04CC4429117DC2C41825E1E9FCA0ADDD34E6F"
+                     "1B39F7B", 16),
+            y_qB=int("990C57520812BE512641E47034832106BC7D3E8DD0E4C7F1136D70065"
+                     "47CEC6A", 16),
+            x_Z=int("89AFC39D41D3B327814B80940B042590F96556EC91E6AE7939BCE31F3A"
+                    "18BF2B", 16),
+            y_Z=int("49C27868F4ECA2179BFD7D59B1E3BF34C1DBDE61AE12931648F43E5963"
+                    "2504DE", 16))
+
+    def test_brainpoolP384r1(self):
+        self._do(
+            curve=curve_brainpoolp384r1,
+            generator=BRAINPOOLP384r1.generator,
+            dA=int("1E20F5E048A5886F1F157C74E91BDE2B98C8B52D58E5003D57053FC4B0B"
+                   "D65D6F15EB5D1EE1610DF870795143627D042", 16),
+            x_qA=int("68B665DD91C195800650CDD363C625F4E742E8134667B767B1B476793"
+                     "588F885AB698C852D4A6E77A252D6380FCAF068", 16),
+            y_qA=int("55BC91A39C9EC01DEE36017B7D673A931236D2F1F5C83942D049E3FA2"
+                     "0607493E0D038FF2FD30C2AB67D15C85F7FAA59", 16),
+            dB=int("032640BC6003C59260F7250C3DB58CE647F98E1260ACCE4ACDA3DD869F7"
+                   "4E01F8BA5E0324309DB6A9831497ABAC96670", 16),
+            x_qB=int("4D44326F269A597A5B58BBA565DA5556ED7FD9A8A9EB76C25F46DB69D"
+                     "19DC8CE6AD18E404B15738B2086DF37E71D1EB4", 16),
+            y_qB=int("62D692136DE56CBE93BF5FA3188EF58BC8A3A0EC6C1E151A21038A42E"
+                     "9185329B5B275903D192F8D4E1F32FE9CC78C48", 16),
+            x_Z=int("0BD9D3A7EA0B3D519D09D8E48D0785FB744A6B355E6304BC51C229FBBC"
+                    "E239BBADF6403715C35D4FB2A5444F575D4F42", 16),
+            y_Z=int("0DF213417EBE4D8E40A5F76F66C56470C489A3478D146DECF6DF0D94BA"
+                    "E9E598157290F8756066975F1DB34B2324B7BD", 16))
+
+    def test_brainpoolP512r1(self):
+        self._do(
+            curve=curve_brainpoolp512r1,
+            generator=BRAINPOOLP512r1.generator,
+            dA=int("16302FF0DBBB5A8D733DAB7141C1B45ACBC8715939677F6A56850A38BD8"
+                   "7BD59B09E80279609FF333EB9D4C061231FB26F92EEB04982A5F1D1764C"
+                   "AD57665422", 16),
+            x_qA=int("0A420517E406AAC0ACDCE90FCD71487718D3B953EFD7FBEC5F7F27E28"
+                     "C6149999397E91E029E06457DB2D3E640668B392C2A7E737A7F0BF044"
+                     "36D11640FD09FD", 16),
+            y_qA=int("72E6882E8DB28AAD36237CD25D580DB23783961C8DC52DFA2EC138AD4"
+                     "72A0FCEF3887CF62B623B2A87DE5C588301EA3E5FC269B373B60724F5"
+                     "E82A6AD147FDE7", 16),
+            dB=int("230E18E1BCC88A362FA54E4EA3902009292F7F8033624FD471B5D8ACE49"
+                   "D12CFABBC19963DAB8E2F1EBA00BFFB29E4D72D13F2224562F405CB8050"
+                   "3666B25429", 16),
+            x_qB=int("9D45F66DE5D67E2E6DB6E93A59CE0BB48106097FF78A081DE781CDB31"
+                     "FCE8CCBAAEA8DD4320C4119F1E9CD437A2EAB3731FA9668AB268D871D"
+                     "EDA55A5473199F", 16),
+            y_qB=int("2FDC313095BCDD5FB3A91636F07A959C8E86B5636A1E930E8396049CB"
+                     "481961D365CC11453A06C719835475B12CB52FC3C383BCE35E27EF194"
+                     "512B71876285FA", 16),
+            x_Z=int("A7927098655F1F9976FA50A9D566865DC530331846381C87256BAF3226"
+                    "244B76D36403C024D7BBF0AA0803EAFF405D3D24F11A9B5C0BEF679FE1"
+                    "454B21C4CD1F", 16),
+            y_Z=int("7DB71C3DEF63212841C463E881BDCF055523BD368240E6C3143BD8DEF8"
+                    "B3B3223B95E0F53082FF5E412F4222537A43DF1C6D25729DDB51620A83"
+                    "2BE6A26680A2", 16))
 
 
 def __main__():
