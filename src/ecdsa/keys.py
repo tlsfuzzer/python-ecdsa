d2b391afa2f9beb114bba507cca761f5aa828b2f
Jonathan Underwood <junderwood@bitcoinbank.co.jp>
2018-08-24 00:57:35 +0900

    Use b'' in keys.py as well

--- src/ecdsa/keys.py
+++ src/ecdsa/keys.py
@@ -225,13 +225,13 @@ class SigningKey:
     def get_verifying_key(self):
         return self.verifying_key
 
-    def sign_deterministic(self, data, hashfunc=None, sigencode=sigencode_string, extra_entropy=b('')):
+    def sign_deterministic(self, data, hashfunc=None, sigencode=sigencode_string, extra_entropy=b''):
         hashfunc = hashfunc or self.default_hashfunc
         digest = hashfunc(data).digest()
 
         return self.sign_digest_deterministic(digest, hashfunc=hashfunc, sigencode=sigencode, extra_entropy=extra_entropy)
 
-    def sign_digest_deterministic(self, digest, hashfunc=None, sigencode=sigencode_string, extra_entropy=b('')):
+    def sign_digest_deterministic(self, digest, hashfunc=None, sigencode=sigencode_string, extra_entropy=b''):
         """
         Calculates 'k' from data itself, removing the need for strong
         random generator and producing deterministic (reproducible) signatures.
