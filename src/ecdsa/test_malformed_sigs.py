8533e51384b786f82c826730cebbcdf579454608
Hubert Kario <hkario@redhat.com>
2019-09-26 18:44:51 +0200

    give the same handling to string encoded signatures as to DER
    Verify that strings of unexpected lengths are rejected with the
    same BadSignatureError exception

--- src/ecdsa/test_malformed_sigs.py
+++ src/ecdsa/test_malformed_sigs.py
@@ -1,28 +1,16 @@
 from __future__ import with_statement, division
 
-import unittest
-import os
-import time
-import shutil
-import subprocess
 import pytest
-from binascii import hexlify, unhexlify
-from hashlib import sha1, sha256, sha512
 import hashlib
 
 from six import b, binary_type
 from .keys import SigningKey, VerifyingKey
 from .keys import BadSignatureError
-from . import util
-from .util import sigencode_der, sigencode_strings
-from .util import sigdecode_der, sigdecode_strings
+from .util import sigencode_der, sigencode_string
+from .util import sigdecode_der, sigdecode_string
 from .curves import curves, NIST256p, NIST521p
-from .ellipticcurve import Point
-from . import der
-from . import rfc6979
-import copy
 
-sigs = []
+der_sigs = []
 example_data = b("some data to sign")
 
 # Just NIST256p with SHA256 is 560 test cases, all curves with all hashes is
@@ -37,14 +25,14 @@ for curve in [NIST256p]:
                              sigencode=sigencode_der)
         for pos in range(len(signature)):
             for xor in (1<<i for i in range(8)):
-                sigs.append(pytest.param(
+                der_sigs.append(pytest.param(
                     key.verifying_key, hash_alg,
                     signature, pos, xor,
                     id="{0}-{1}-pos-{2}-xor-{3}".format(
                         curve.name, hash_alg, pos, xor)))
 
 
-@pytest.mark.parametrize("verifying_key,hash_alg,signature,pos,xor", sigs)
+@pytest.mark.parametrize("verifying_key,hash_alg,signature,pos,xor", der_sigs)
 def test_fuzzed_der_signatures(verifying_key, hash_alg, signature, pos, xor):
     # check if a malformed DER encoded signature causes the same exception
     # to be raised irrespective of the type of error
@@ -60,9 +48,40 @@ def test_fuzzed_der_signatures(verifying_key, hash_alg, signature, pos, xor):
         assert True
 
 
-def __main__():
-    unittest.main()
+####
+#
+# For string encoded signatures, only the length of string is important
+#
+####
+
+str_sigs = []
+
+#for curve in curves:
+for curve in [NIST256p]:
+    #for hash_alg in ["md5", "sha1", "sha224", "sha256", "sha384", "sha512"]:
+    for hash_alg in ["sha256"]:
+        key = SigningKey.generate(curve)
+        signature = key.sign(example_data, hashfunc=getattr(hashlib, hash_alg),
+                             sigencode=sigencode_string)
+        for trunc in range(len(signature)):
+            str_sigs.append(pytest.param(
+                key.verifying_key, hash_alg,
+                signature, trunc,
+                id="{0}-{1}-trunc-{2}".format(
+                    curve.name, hash_alg, trunc)))
 
 
-if __name__ == "__main__":
-    __main__()
+@pytest.mark.parametrize("verifying_key,hash_alg,signature,trunc", str_sigs)
+def test_truncated_string_signatures(verifying_key, hash_alg, signature, trunc):
+    # check if a malformed string encoded signature causes the same exception
+    # to be raised irrespective of the type of error
+    sig = bytearray(signature)
+    sig = sig[:trunc]
+    sig = binary_type(sig)
+
+    try:
+        verifying_key.verify(sig, example_data, getattr(hashlib, hash_alg),
+                             sigdecode_string)
+        assert False
+    except BadSignatureError:
+        assert True
