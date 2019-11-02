aa7e7b36b9eedecbbc0698e80ea9d59882e95bed
Hubert Kario <hubert@kario.pl>
2019-11-02 19:24:22 +0100

    add quick mutation testing to travis

--- cosmic-ray.sh
+++ cosmic-ray.sh
@@ -7,3 +7,4 @@ cr-report --show-output session.baseline.sqlite
 cosmic-ray exec session.sqlite
 cr-report session.sqlite
 cr-html session.sqlite > session.html
+cr-rate --estimate --fail-over 25 --confidence 99.9 session.sqlite
