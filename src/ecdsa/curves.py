5e1afee856df82fe9eb63d07d8205626418baeb7
Hubert Kario <hubert@kario.pl>
2019-11-12 01:37:11 +0100

    curves: pep8 fixes

--- src/ecdsa/curves.py
+++ src/ecdsa/curves.py
@@ -63,36 +63,50 @@ NIST521p = Curve("NIST521p", ecdsa.curve_521,
 SECP256k1 = Curve("SECP256k1", ecdsa.curve_secp256k1,
                   ecdsa.generator_secp256k1,
                   (1, 3, 132, 0, 10), "secp256k1")
+
+
 BRAINPOOLP160r1 = Curve("BRAINPOOLP160r1",
                         ecdsa.curve_brainpoolp160r1,
                         ecdsa.generator_brainpoolp160r1,
                         (1, 3, 36, 3, 3, 2, 8, 1, 1, 1),
                         "brainpoolP160r1")
+
+
 BRAINPOOLP192r1 = Curve("BRAINPOOLP192r1",
                         ecdsa.curve_brainpoolp192r1,
                         ecdsa.generator_brainpoolp192r1,
                         (1, 3, 36, 3, 3, 2, 8, 1, 1, 3),
                         "brainpoolP192r1")
+
+
 BRAINPOOLP224r1 = Curve("BRAINPOOLP224r1",
                         ecdsa.curve_brainpoolp224r1,
                         ecdsa.generator_brainpoolp224r1,
                         (1, 3, 36, 3, 3, 2, 8, 1, 1, 5),
                         "brainpoolP224r1")
+
+
 BRAINPOOLP256r1 = Curve("BRAINPOOLP256r1",
                         ecdsa.curve_brainpoolp256r1,
                         ecdsa.generator_brainpoolp256r1,
                         (1, 3, 36, 3, 3, 2, 8, 1, 1, 7),
                         "brainpoolP256r1")
+
+
 BRAINPOOLP320r1 = Curve("BRAINPOOLP320r1",
                         ecdsa.curve_brainpoolp320r1,
                         ecdsa.generator_brainpoolp320r1,
                         (1, 3, 36, 3, 3, 2, 8, 1, 1, 9),
                         "brainpoolP320r1")
+
+
 BRAINPOOLP384r1 = Curve("BRAINPOOLP384r1",
                         ecdsa.curve_brainpoolp384r1,
                         ecdsa.generator_brainpoolp384r1,
                         (1, 3, 36, 3, 3, 2, 8, 1, 1, 11),
                         "brainpoolP384r1")
+
+
 BRAINPOOLP512r1 = Curve("BRAINPOOLP512r1",
                         ecdsa.curve_brainpoolp512r1,
                         ecdsa.generator_brainpoolp512r1,
