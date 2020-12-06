cbf086e602f04c873b64b4b4f0a3174db8bb3857
Hubert Kario <hubert@kario.pl>
2020-12-06 17:46:00 +0100

    how to run cosmic-ray comfortably

--- cosmic-ray.sh
+++ cosmic-ray.sh
@@ -4,6 +4,8 @@ set -e
 cosmic-ray init cosmic-ray.toml session.sqlite
 cosmic-ray baseline --report session.sqlite
 cr-report --show-output session.baseline.sqlite
+# some mutations cause huge memory use, so put it in a cgroup
+# systemd-run --user --scope -p MemoryMax=2G -p MemoryHigh=2G cosmic-ray exec session.sqlite
 cosmic-ray exec session.sqlite
 cr-report session.sqlite
 cr-html session.sqlite > session.html
