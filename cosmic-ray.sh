ef90135f415fe6a576048bf570391d76f7318ec7
Hubert Kario <hubert@kario.pl>
2020-12-06 20:40:43 +0100

    increase the survival rate limit
    currently we have 28.88% survival rate, so set that as the baseline

--- cosmic-ray.sh
+++ cosmic-ray.sh
@@ -9,4 +9,4 @@ cr-report --show-output session.baseline.sqlite
 cosmic-ray exec session.sqlite
 cr-report session.sqlite
 cr-html session.sqlite > session.html
-cr-rate --estimate --fail-over 25 --confidence 99.9 session.sqlite
+cr-rate --estimate --fail-over 29 --confidence 99.9 session.sqlite
